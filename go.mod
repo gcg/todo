a7cfae00c68c2b74639b27322c9248239cd45d3c
gcg <guneycan@gmail.com>
2022-01-23 14:33:10 +0300

    Get flex and read file

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/gcg/todo
 
 go 1.16
 
-require github.com/rivo/tview v0.0.0-20220106183741-90d72bc664f5
+require (
+	github.com/gdamore/tcell/v2 v2.4.1-0.20210905002822-f057f0a857a1
+	github.com/rivo/tview v0.0.0-20220106183741-90d72bc664f5
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
+)
--- main.go
+++ main.go
@@ -1,12 +1,62 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
+	"gopkg.in/yaml.v3"
 )
 
 func main() {
-	box := tview.NewBox().SetBorder(true).SetTitle("Todo Items")
-	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
+	// create the main app
+	app := tview.NewApplication()
+
+	// create a flex layout so we can put more stuff more neatly
+	flex := tview.NewFlex().SetDirection(tview.FlexRow)
+
+	flex.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Rune() == 'q' {
+			app.Stop()
+			return nil
+		}
+		return event
+	})
+
+	// add a box for the content
+
+	list := tview.NewList()
+
+	list.SetBorder(true).SetTitle(" Todo Items ")
+	flex.AddItem(list, 0, 8, true)
+
+	// add a box for the footer/help
+	footerBox := tview.NewTextView()
+
+	fmt.Fprint(footerBox, "To quit press (q), to add new item press (n) and to mark/unmark a task press (space bar)")
+	footerBox.SetBorder(true).SetTitle(" Help ?")
+	flex.AddItem(footerBox, 0, 1, false)
+
+	// create the list
+
+	// read or create the yaml file.
+	wd, err := os.Getwd()
+	if err != nil {
 		panic(err)
 	}
+	file, err := os.ReadFile(fmt.Sprintf("%s/%s", wd, "todo.yaml"))
+	if err != nil {
+		panic(err)
+	}
+
+	err = yaml.Unmarshal(file, &list)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := app.SetRoot(flex, true).SetFocus(flex).Run(); err != nil {
+		panic(err)
+	}
+
 }
